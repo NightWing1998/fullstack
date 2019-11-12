93decfdef244e50ee3878509dfb244dc02ebb856
NightWing1998 <dhruvil.s@somaiya.edu>
2019-11-12 19:33:18 +0530

    4.3 done

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,5 +1,7 @@
 const dummy = (blogs) => 1;
 
+const totalLikes = (blogs) => blogs.length === 0 ? 0 : blogs.reduce((previousValue, currentValue) => (previousValue + currentValue.likes), 0)
+
 module.exports = {
 	dummy
 }
