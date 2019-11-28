fbf7892af41bf1cc22ce7e399c2acf62e216f63f
NightWing1998 <dhruvil.s@somaiya.edu>
2019-11-28 23:43:04 +0530

    error handling for user not existing in database

--- part4/blog/controllers/index.js
+++ part4/blog/controllers/index.js
@@ -18,14 +18,20 @@ router.post("/login", async (req, res, next) => {
 		const savedUser = await User.findOne({
 			username
 		});
-		console.log(">>>", bcrypt.compareSync(password, savedUser.passwordHash));
+		if (savedUser === null) {
+			return res.status(400).json({
+				error: "Username does not exist."
+			});
+		};
+
 		if (savedUser !== null && bcrypt.compareSync(password, savedUser.passwordHash)) {
 			const token = jwt.sign({
 				username,
 				id: savedUser._id
 			}, config.JWT_SECRET);
 			res.status(201).json({
-				token
+				token,
+				username
 			});
 		} else {
 			res.status(401).json({
