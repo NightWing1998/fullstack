36101a49553e6ae84bef7173133ee7f0d5635a68
NightWing1998 <dhruvil.s@somaiya.edu>
2019-12-21 17:14:15 +0530

    webpack tried

---
+++ part7/bloglist-expanded/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path")
+
+const config = {
+	entry: ["@babel/polyfill", "./src/index.js"],
+	output: {
+		path: path.resolve(__dirname, "build"),
+		filename: "main.js"
+	},
+	resolve: {
+		extensions: ['.js', '.jsx']
+	},
+	module: {
+		rules: [{
+				test: /\.js*$/,
+				loader: "babel-loader",
+				query: {
+					presets: ["@babel/preset-env", "@babel/preset-react"],
+					presets: ['react']
+				},
+			},
+			{
+				test: /\.css$/,
+				loaders: ["style-loader", "css-loader"],
+			},
+		],
+	},
+	devServer: {
+		contentBase: path.resolve(__dirname, "build"),
+		compress: true,
+		port: 3000,
+	},
+	devtool: "source-map"
+}
+
+module.exports = config;
